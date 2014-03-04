1426a16775a598e06211a24aabc49abee65345af
Stuart <gravatar@strudel.org.uk>
2014-03-04 18:28:15 +0000

    Re-work how units/values are dealt with

--- js/spacetelescope.js
+++ js/spacetelescope.js
@@ -111,7 +111,7 @@ if(typeof $==="undefined") $ = {};
 		this.langurl = "config/%LANG%.json";
 		this.dataurl = "config/options.json";
 		
-		this.settings = { 'length': 'm', 'currency': 'GBP', 'usecookies': false };
+		this.settings = { 'length': 'm', 'currency': 'GBP', 'temperature':'K', 'mass':'kg', 'time': 'months', 'usecookies': false };
 
 		this.init(inp);
 
@@ -130,40 +130,43 @@ if(typeof $==="undefined") $ = {};
 		this.setVar("currency","string");
 		this.setVar("length","string");
 		this.setVar("mass","string");
+		this.setVar("temperature","string");
+		this.setVar("time","string");
 
 		this.loadConfig(this.update);
 		this.loadLanguage(this.lang,this.update);
 		
 		// Build language list
 		var list = "";
-		for(l in this.langs) list += '<li class="baritem"><a href="#" title="'+this.langs[l]+'">'+this.langs[l]+' ['+l+']</a></li>'
+		for(l in this.langs) list += '<li class="baritem"><a href="#" title="'+this.langs[l]+'" data="'+l+'">'+this.langs[l]+' ['+l+']</a></li>'
 		$('#language ul').html(list);
+		// Redefine list variable to be the jQuery DOM object
+		list = $('#language ul li a');
+		// Add click events to each language in the list
+		for(var i = 0; i < list.length; i++) $(list[i]).on('click',{me:this},function(e){ e.data.me.loadLanguage($(this).attr('data'),e.data.me.update); });
+
 
 		// Make menu toggles active
 		$('#summary a').on('click',{me:this},function(e){ e.data.me.toggleMenus('summaryext'); });
-		$('a.togglemessages').on('click',{me:this},function(e){ e.data.me.toggleMenus('messages'); });
+		$('a.togglewarnings,a.toggleerrors').on('click',{me:this},function(e){ e.data.me.toggleMenus('messages'); });
 		$('a.togglemenu').on('click',{me:this},function(e){ e.data.me.toggleMenus('menu'); });
 		$('a.togglelang').on('click',{me:this},function(e){ e.data.me.toggleMenus('language'); });
 
 
-		// Add save event
-		$('.baritem .save').parent().on('click',{me:this},function(e){
-			e.data.me.save();
-		});
-
-
-		// Check if in an iframe
+		// Add main menu events
+		$('.baritem .save').parent().on('click',{me:this},function(e){ e.data.me.save().toggleMenus(); });
+		$('.baritem .help').parent().on('click',{me:this},function(e){ e.data.me.toggleGlossary().toggleMenus(); });
 		if(fullScreenApi.supportsFullScreen){
-			$('#menu ul').append('<li class="baritem"><a href="#" class="fullscreenbtn"><img src="images/cleardot.gif" class="icon fullscreen" alt="full" /> <span>Enter Fullscreen</span></li>');
+			// Add the fullscreen toggle to the menu
+			$('#menu ul').append('<li class="baritem" data="fullscreen"><a href="#" class="fullscreenbtn"><img src="images/cleardot.gif" class="icon fullscreen" alt="full" /> <span></span></li>');
 			// Bind the fullscreen function to the double-click event if the browser supports fullscreen
-			$('#menu li.baritem a.fullscreenbtn').on('click', {me:this}, function(e){
-				e.data.me.toggleFullScreen();
-			});
+			$('.baritem .fullscreenbtn').parent().on('click', {me:this}, function(e){ e.data.me.toggleFullScreen().toggleMenus(); });
 		}
 
 
 		return this;
 	}
+
 	SpaceTelescope.prototype.toggleMenus = function(id){
 		var m = $('.dropdown');
 		for(var i = 0; i < m.length ; i++){
@@ -253,6 +256,7 @@ if(typeof $==="undefined") $ = {};
 		if(!this.phrases || !this.data) return this;
 
 		this.updateLanguage();
+		$('#language.dropdown').hide()
 		
 		return this;
 	}
@@ -273,9 +277,9 @@ if(typeof $==="undefined") $ = {};
 		$('body').attr('dir',(d.language.alignment=="right" ? 'rtl' : 'ltr')).removeClass('ltr rtl').addClass((d.language.alignment=="right" ? 'rtl' : 'ltr'));
 
 		var html = "<ul>";
-		console.log(this.data.rocket)
+
 		for(var l in this.data.rocket){
-			html += '<li><div class="rocket">'+this.phrases.options.rocket[l].label+'</div><div class="operator"><img src="'+this.data.operator[this.data.rocket[l].operator].img+'" alt="'+this.phrases.options.operator[this.data.rocket[l].operator].label+'" title="'+this.phrases.options.operator[this.data.rocket[l].operator].label+'"></div><div class="diameter"><strong>Diameter:</strong> '+this.formatLength(this.data.rocket[l].diameter)+'</div> <div class="currency"><strong>Cost:</strong> '+this.formatCurrency(this.data.rocket[l].cost)+'</div><div class="mass"><strong>Mass to LEO:</strong> '+this.formatMass(this.data.rocket[l].mass.LEO)+'<br /><strong>Mass beyond LEO:</strong> '+this.formatMass(this.data.rocket[l].mass.GTO)+'</div><div class="risk"><strong>Risk:</strong> '+(this.data.rocket[l].risk)+'</div>'
+			html += '<li><div class="rocket">'+this.phrases.options.rocket[l].label+'</div><div class="operator"><img src="'+this.data.operator[this.data.rocket[l].operator].img+'" alt="'+this.phrases.options.operator[this.data.rocket[l].operator].label+'" title="'+this.phrases.options.operator[this.data.rocket[l].operator].label+'"></div><div class="diameter"><strong>Diameter:</strong> '+this.formatValue(this.data.rocket[l].diameter)+'</div> <div class="currency"><strong>Cost:</strong> '+this.formatValue(this.data.rocket[l].cost)+'</div><div class="mass"><strong>Mass to LEO:</strong> '+this.formatMass(this.data.rocket[l].mass.LEO)+'<br /><strong>Mass beyond LEO:</strong> '+this.formatMass(this.data.rocket[l].mass.GTO)+'</div><div class="risk"><strong>Risk:</strong> '+(this.data.rocket[l].risk)+'</div>'
 			html += '<div class="sites"><strong>Sites:</strong> <br />'
 			for(var i = 0; i < this.data.rocket[l].sites.length; i++){
 				html += ''+this.phrases.options.site[this.data.rocket[l].sites[i]].label+'<br />';
@@ -285,59 +289,187 @@ if(typeof $==="undefined") $ = {};
 		html += "</ul>";
 		$('#output').html(html);
 
+		// Update menu
+		$('.togglemenu').attr('title',d.ui.menu.title);
+		$('#menu ul li').each(function(i){
+			var id = $(this).attr('data');
+			$(this).find('span').html(d.ui.menu[id])
+		})
+
+
+		$('.togglewarnings').attr('title',d.ui.warnings.title);
+		$('.toggleerrors').attr('title',d.ui.errors.title);
+
+		$('.togglesuccess').attr('title',d.ui.summary.success.title);
+		$('.toggletime').attr('title',d.ui.summary.time.title);
+		$('.togglecost').attr('title',d.ui.summary.cost.title);
+		$('.togglemass').attr('title',d.ui.summary.mass.title);
+		$('.togglescience').attr('title',d.ui.summary.science.title);
+
+		// Update introduction text
+		$('#introduction').html(d.intro);
+
 		return this;
 	}
 
+	// Convert an input value/unit/dimension object into another unit
 	// Inputs:
-	//  n - the number
-	//  u - the unit e.g. "m" or "ft"
+	//  v - the { "value": 1, "units": "m", "dimension": "length" } object
+	//  to - the new unit (must be one of the acceptable units for the dimension
+	SpaceTelescope.prototype.convertValue = function(v,to){
+
+		// If the input isn't the sort of thing we expect we just return it
+		if(typeof v != "object") return v;
+
+		if(typeof v.value==="string") v.value = parseFloat(v.value,10);
+
+		if(v.dimension == "length"){
+			if(to == "ft" || to == "m" || to == "mm" || to == "km"){
+				if(v.units != to){
+					// Step 1: convert to SI
+					if(v.units == "ft") v.value /= 3.281;
+					else if(v.units == "mm") v.value /= 1000;
+					else if(v.units == "km") v.value /= 0.001;
+					// Step 2: convert to new unit
+					if(to == "ft") v.value *= 3.281;
+					else if(to == "mm") v.value *= 1000;
+					else if(to == "km") v.value *= 0.001;
+					v.units = to;
+				}
+			}
+		}else if(v.dimension == "mass"){
+			if(to == "kg" || to == "lb" || to == "t"){
+				if(v.units != to){
+					// Step 1: convert to SI
+					if(v.units == "t") v.value /= 0.001;
+					else if(v.units == "lb") v.value /= 2.205;
+					// Step 2: convert to new unit
+					if(to == "t") v.value *= 0.001;
+					else if(to == "lb") v.value *= 2.205;
+					v.units = to;
+				}
+			}
+		}else if(v.dimension == "temperature"){
+			if(to == "K" || to == "C" || to == "F"){
+				if(v.units != to){
+					// Step 1: convert to SI
+					if(v.units == "C") v.value += 273;
+					else if(v.units == "F") v.value = (5/9)*(v.value - 32) + 273;
+					// Step 2: convert to new unit
+					if(to == "C") v.value -= 273;
+					else if(to == "F") v.value = (9/5)*(v.value - 273) + 32;
+					v.units = to;
+				}
+			}
+		}else if(v.dimension == "time"){
+			if(to == "years" || to == "months" || to == "days"){
+				if(v.units != to){
+					// Step 1: convert to SI
+					if(v.units == "months") v.value /= 12;
+					else if(v.units == "days") v.value /= 365.25;
+					// Step 2: convert to new unit
+					if(to == "months") v.value *= 12;
+					else if(to == "days") v.value *= 365.25;
+					v.units = to;
+				}
+			}
+		}else if(v.dimension == "currency"){
+			if(this.data.currency[to]){
+				if(v.units != to){
+					// Step 1: convert to GBP
+console.log(v.units,to,v)
+					if(this.data.currency[v.units].conv) v.value /= this.data.currency[v.units].conv;
+					// Step 2: convert to new unit
+					if(this.data.currency[to].conv) v.value *= this.data.currency[to].conv;
+					v.units = to;
+				}
+			}
+		}
+		
+		return v;
+	}
+
+	// Format a value. This calls the appropriate format function for the dimension
+	// Inputs:
+	//  v - the value as an object e.g. { "value": 1, "units": "m", "dimension": "length" }
 	//  p - the number of decimal places to show in the output
-	SpaceTelescope.prototype.formatLength = function(n,u,p){
-		if(typeof n==="string") n = parseFloat(n,10);
-		if(!u) u = (this.settings.length) ? this.settings.length : "m";
+	SpaceTelescope.prototype.formatValue = function(v,p){
+		if(typeof v==="string") return v;
+		if(typeof v==="object" && v.value){
+			if(v.dimension=="length") return this.formatLength(v,p);
+			else if(v.dimension=="mass") return this.formatMass(v,p)
+			else if(v.dimension=="currency") return this.formatCurrency(v,p)
+			else if(v.dimension=="temperature") return this.formatTemperature(v,p)
+			else if(v.dimension=="time") return this.formatTime(v,p)
+			else return v.value;
+		}
+		
+		return "";
+	}
+
+	// Inputs:
+	//  v - the { "value": 1, "units": "m", "dimension": "length" } object
+	//  p - the number of decimal places to show in the output
+	SpaceTelescope.prototype.formatLength = function(v,p){
+		v = this.convertValue(v,(this.settings.length) ? this.settings.length : "m")
 		if(typeof p==="string") p = parseInt(p,10);
 		if(!p) p = 1;
-		var unit = (this.phrases.ui.units[u]) ? this.phrases.ui.units[u].unit : "";
-		var conv = (this.data.units[u].conv) ? this.data.units[u].conv : 1;
-		return ''+addCommas((n*conv).toFixed(p))+''+unit;
+		var unit = (this.phrases.ui.units[v.units]) ? this.phrases.ui.units[v.units].unit : "";
+		return ''+addCommas((v.value).toFixed(p))+''+unit;
 	}
 
 	// Inputs:
-	//  n - the number
-	//  u - the unit e.g. "kg" or "lb"
+	//  v - the { "value": 1, "units": "m", "dimension": "length" } object
 	//  p - the number of decimal places to show in the output
-	SpaceTelescope.prototype.formatMass = function(n,u,p){
-		if(typeof n==="string") n = parseFloat(n,10);
-		if(!u) u = (this.settings.mass) ? this.settings.mass : "kg";
+	SpaceTelescope.prototype.formatMass = function(v,p){
+		v = this.convertValue(v,(this.settings.mass) ? this.settings.mass : "kg")
 		if(typeof p==="string") p = parseInt(p,10);
-		var unit = (this.phrases.ui.units[u]) ? this.phrases.ui.units[u].unit : "";
-		var conv = (this.data.units[u].conv) ? this.data.units[u].conv : 1;
-		n *= conv;
-		if(!p){
-			p = (n > 1000) ? 0 : 1;
-		}
-		return ''+addCommas((n).toFixed(p))+''+unit;
+		var unit = (this.phrases.ui.units[v.units]) ? this.phrases.ui.units[v.units].unit : "";
+		if(!p) p = (v.value > 1000) ? 0 : 1;
+		return ''+addCommas((v.value).toFixed(p))+''+unit;
 	}
 
 	// Inputs:
-	//  n - the number
-	//  c - the currency e.g. "GBP", "USD", "EUR"
+	//  v - the { "value": 1, "units": "m", "dimension": "length" } object
 	//  p - the number of decimal places to show in the output
-	SpaceTelescope.prototype.formatCurrency = function(n,c,p){
-		if(typeof n==="string") n = parseFloat(n,10);
-		if(!c) c = (this.settings.currency) ? this.settings.currency : "GBP";
+	SpaceTelescope.prototype.formatCurrency = function(v,p){
+		v = this.convertValue(v,(this.settings.currency) ? this.settings.currency : "GBP")
 		if(typeof p==="string") p = parseInt(p,10);
 		if(!p) p = 0;
-		var append = (this.phrases.ui.million) ? this.phrases.ui.million : "";
-		var s = (this.phrases.ui.currency[c] && this.phrases.ui.currency[c].symbol) ? this.phrases.ui.currency[c].symbol : (this.phrases.ui.currency["GBP"].symbol ? this.phrases.ui.currency["GBP"].symbol : "");
-		var conv = (this.data.currency[c] && this.data.currency[c].conv) ? this.data.currency[c].conv : 1;
-		n *= conv;
+
+
+		var append = (this.phrases.ui.million.compact) ? this.phrases.ui.million.compact : "";
+		var s = (this.phrases.ui.currency[v.units] && this.phrases.ui.currency[v.units].symbol) ? this.phrases.ui.currency[v.units].symbol : (this.phrases.ui.currency["GBP"].symbol ? this.phrases.ui.currency["GBP"].symbol : "");
+
 		// Change the "million" to "billion" if the number if too big
-		if(n >= 1000){
-			n /= 1000;
-			append = (this.phrases.ui.billion) ? this.phrases.ui.billion : "";
+		if(v.value >= 1000){
+			v.value /= 1000;
+			append = (this.phrases.ui.billion.compact) ? this.phrases.ui.billion.compact : "";
 		}
-		return s+''+(n).toFixed(p)+''+append;
+		return s+''+(v.value).toFixed(p)+''+append;
+	}
+
+
+	// Inputs:
+	//  v - the { "value": 1, "units": "m", "dimension": "length" } object
+	//  p - the number of decimal places to show in the output
+	SpaceTelescope.prototype.formatTemperature = function(v,p){
+		v = this.convertValue(v,(this.settings.temperature) ? this.settings.temperature : "K")
+		if(typeof p==="string") p = parseInt(p,10);
+		if(!p) p = (v.value > 1000) ? 0 : 1;
+		var unit = (this.phrases.ui.units[v.units]) ? this.phrases.ui.units[v.units].unit : "";
+		return ''+addCommas((v.value).toFixed(p))+''+unit;
+	}
+
+	// Inputs:
+	//  v - the { "value": 1, "units": "m", "dimension": "length" } object
+	//  p - the number of decimal places to show in the output
+	SpaceTelescope.prototype.formatTime = function(v,p){
+		v = this.convertValue(v,(this.settings.time) ? this.settings.time : "months")
+		if(typeof p==="string") p = parseInt(p,10);
+		if(!p) p = (v.value >= 6) ? 0 : 1;
+		var unit = (this.phrases.ui.units[v.units]) ? this.phrases.ui.units[v.units].unit : "";
+		return ''+addCommas((v.value).toFixed(p))+''+unit;
 	}
 
 	SpaceTelescope.prototype.toggleFullScreen = function(){
@@ -354,8 +486,82 @@ if(typeof $==="undefined") $ = {};
 			$('#page').addClass('fullscreen');
 		}
 		//this.resize();
+		return this;
 	}
 
+	SpaceTelescope.prototype.toggleGlossary = function(key){
+
+		var html = "";
+		var g = this.phrases.glossary;
+		var v,ul;
+		$('#intro').hide();	
+		if($('#glossary').length == 0) $('#page').append('<div id="glossary"></div>')
+
+		// Remove any existing events attached to glossary links
+		$('#glossary a.glossarylink').off('click');
+
+		if(key){
+			html += '<p class="breadcrumb"><a href="#glossary" class="glossarylink" data="">'+g.title+'</a> &raquo; '+g[key].title+'</p>'
+			html += '<h2>'+g[key].title+'</h2><p>'+g[key].about+'</p>';
+			if(key=="roles"){
+				for(k in g[key]){
+					if(k != "title" && k != "about" && typeof g[key][k].title==="string"){
+						html += '<h3>'+g[key][k].title+'</h3><p>'+g[key][k].about+'</p>';
+					}
+				}
+			}else if(key=="previous"){
+				/*"spitzer": {
+					"title": "Spitzer Space Telescope",
+					"launch": 2003,
+					"operator": "NASA",
+					"duration": { "value": 5.5, "unit": "years" },
+					"instruments": "Mid-IR, Near-IR",
+					"temperature": { "value": 5, "unit": "K" },
+					"diameter": { "value": 0.85, "unit": "m" },
+					"mass": { "value": 860, "unit": "kg" },
+					"site": "Cape Canaveral, Florida, USA",
+					"vehicle": "Delta II rocket",
+					"orbit": "Earth-trailing orbit",
+					"cost": { "value": 800000000, "unit": "GBP" },
+					"notes": "Since further cooling is only required by the Mid-IR instruments, the Near-IR instruments continued to operate after the end of the nominal mission."
+				},*/
+
+				for(k in g[key]["missions"]){
+					if(typeof g[key]["missions"][k].title==="string"){
+						ul = '<ul>';
+						for(i in g[key]["missions"][k]){
+							if(i != "title" && i != "image"){
+								v = this.formatValue(g[key]["missions"][k][i]);
+								ul += '<li><span class="key">'+g[key][i]+'</span> <span class="value">'+v+'</span></li>';
+							}
+						}
+						ul += '</ul>';
+						html += '<div id="'+k+'" class="mission"><h3>'+g[key]["missions"][k].title+'</h3>';
+						if(g[key]["missions"][k]["image"]) html += '<img src="'+g[key]["missions"][k]["image"].src+'" alt="'+g[key]["missions"][k]["title"]+'" />';
+						html += '</div>';
+						html += ul;
+					}
+				}
+			}
+		}else{
+		
+			html += '<h2>'+this.phrases.glossary.title+'</h2><p>'+this.phrases.glossary.about+'</p><ul>';
+			for(k in this.phrases.glossary){
+				if(k != "title" && k != "about" && typeof this.phrases.glossary[k].title==="string"){
+					html += '<li><a href="#glossary" class="glossarylink" data="'+k+'">'+this.phrases.glossary[k].title+'</a></li>';
+				}
+			}
+			html += '</ul>'
+		}
+		$('#glossary').html(html);
+		
+		// Add events to glossary
+		$('#glossary a.glossarylink').on('click',{me:this},function(e){
+			e.data.me.toggleGlossary($(this).attr('data'));
+		});
+
+		return this;
+	}
 
 	// Attempt to save a file
 	// Blob() requires browser >= Chrome 20, Firefox 13, IE 10, Opera 12.10 or Safari 6
