6e67698f36606a55b324a4cb78bc7cc0bd078100
Stuart <gravatar@strudel.org.uk>
2015-04-14 12:24:11 +0100

    Spell check

--- js/spacetelescope.js
+++ js/spacetelescope.js
@@ -278,7 +278,7 @@ if(typeof $==="undefined") $ = {};
 		if(is(Blob,"function")){
 			// Add load to menu
 			$('#menu ul').append('<li class="baritem" data="load"><a href="#"><img src="images/cleardot.gif" class="icon load" alt="" /><span>Load</span></a><input type="file" id="files" name="files[]" multiple /></li>');
-			this.log("Blog exists. :)")
+			this.log("Blob exists. :)")
 			$(document).on('change','#files',{me:this},function(e){
 				e.data.me.log("List of files has changed. Trying to call readFile()")
 				readFile(e.target.files,e.data.me);
